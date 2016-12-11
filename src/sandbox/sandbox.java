59657083756ad457a4a7655c72859951ff6811c4
matisla <herrmann.matthieu@gmail.com>
2016-12-11 23:13:11 +0100

    -

--- src/sandbox/sandbox.java
+++ src/sandbox/sandbox.java
@@ -1,9 +1,14 @@
 package sandbox;
 
+import view.MainFrame;
+
 public class sandbox {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		MainFrame main = new MainFrame();
+		main.setVisible(true);
 	}
 	
 	public int addition(int a, int b){
